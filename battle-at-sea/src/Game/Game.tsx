89ee0ce84f10fdc4f80fc4daa011cf79a34582cb
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2024-09-22 01:55:47 +0000

    start the shell of the project

---
+++ battle-at-sea/src/Game/Game.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from '../Boad/Board';
+
+function Game() {
+  return (
+    <div>
+      <h1>Battle-At-Sea</h1>
+       <Board />   
+       <Board />   
+    </div>
+  );
+}
+
+export default Game;
